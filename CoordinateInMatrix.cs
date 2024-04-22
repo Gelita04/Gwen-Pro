0de53a29e2308cd944282fcadf6d38e2b8bf85a4
Magela_Cornelio <gelacornelio3@gmail.com>
2024-04-22 00:38:57 -0400

    Photos added, methods fixes,

---
+++ CoordinateInMatrix.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinateInMatrix : MonoBehaviour
+{
+    public int coordinateXInMatrixBoard;
+    public int coordinateYInMatrixBoard;
+}
